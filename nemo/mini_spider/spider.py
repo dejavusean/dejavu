b7e566eb6ade9c849148977720b45e0687ed7ff7
wangcongjun <ccking@126.com>
2015-02-16 16:24:22 +0800

    my spider

---
+++ nemo/mini_spider/spider.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015Äê2ÔÂ16ÈÕ
+
+@author: wangcongjun
+'''
+
+class Spider(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
