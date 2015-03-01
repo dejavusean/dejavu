c31ba18889ae2d23a934bd0163ae972ef8b9bdc0
dejavusean <ccking@126.com>
2015-03-01 21:37:01 +0800

    first

--- nemo/mini_spider/spider.py
+++ nemo/mini_spider/spider.py
@@ -1,17 +1,28 @@
+
 '''
-Created on 2015Äê2ÔÂ16ÈÕ
 
 @author: wangcongjun
 '''
-
+import requests
+from bs4 import BeautifulSoup
 class Spider(object):
     '''
     classdocs
     '''
 
 
-    def __init__(self, params):
+    def __init__(self):
         '''
         Constructor
         '''
-        print("hello")
+    def getInfo(self):
+
+        response = requests.get("http://jecvay.com")
+        soup = BeautifulSoup(response.text)
+        for x in soup.findAll("a"):
+            print(x['href'])
+#         print(soup.body.text)
+        
+if __name__ == '__main__':
+    s = Spider()
+    s.getInfo()
---
+++ nemo/mini_spider/test.py
@@ -0,0 +1,8 @@
+'''
+Created on 2015Äê3ÔÂ1ÈÕ
+
+@author: wang
+'''
+
+if __name__ == '__main__':
+    pass
